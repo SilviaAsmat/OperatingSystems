f60048e6048adc105748e644a866938a245940fa
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-12-01 20:29:59 -0600

    Added OPT methods

--- VirtualMemory/OPTAlgorithm.java
+++ VirtualMemory/OPTAlgorithm.java
@@ -1,41 +1,73 @@
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class OPTAlgorithm implements PageReplacementAlgorithm {
-
     private int pageFaults;
+    private List<Integer> frames;
+    private List<Integer> pageReferenceString;
+
+    OPTAlgorithm() {
+        pageFaults = 0;
+    }
+
 
     @Override
-    public void applyAlgorithm(List<Integer> pageReferenceString, int numberOfFrames) {
-        pageFaults = 0;
-        Set<Integer> frames = new HashSet<>(numberOfFrames);
-
-        for (int i = 0; i < pageReferenceString.size(); i++) {
-            int page = pageReferenceString.get(i);
-            if (!frames.contains(page)) {
-                if (frames.size() == numberOfFrames) {
-                    int farthest = i;
-                    int pageToRemove = -1;
-                    for (int frame : frames) {
-                        int nextUse = pageReferenceString.subList(i, pageReferenceString.size()).indexOf(frame);
-                        if (nextUse == -1) {
-                            pageToRemove = frame;
-                            break;
-                        } else if (nextUse > farthest) {
-                            farthest = nextUse;
-                            pageToRemove = frame;
-                        }
-                    }
-                    frames.remove(pageToRemove);
-                }
-                frames.add(page);
+    public void applyAlgorithm(List<Integer> pageReferenceString, int numberOfFrames) 
+    {
+        int indexOfFurthestFrame;
+        this.pageReferenceString = pageReferenceString;
+        frames = new ArrayList<>(numberOfFrames);
+        for(int i = 0; i < numberOfFrames; i++)
+        {
+            frames.add(pageReferenceString.get(i));
+            pageFaults++;
+        }
+        for(int i = numberOfFrames; i < pageReferenceString.size(); i++)
+        {
+            if(!frames.contains(pageReferenceString.get(i)))
+            {
+                indexOfFurthestFrame = findFurtherestOccurrence();
+                replaceFrame(indexOfFurthestFrame, i);
                 pageFaults++;
             }
         }
     }
 
+    public void replaceFrame(int indexOfFurthestFrame, int indexOfNewFrame)
+    {
+        frames.set(indexOfFurthestFrame, pageReferenceString.get(indexOfNewFrame));
+    }
+
+    public int findFurtherestOccurrence()
+    {
+        // for(int i = numberOfFrames; i < pageReferenceString.size(); i++)
+        // {
+        //     int lastOccurrence = pageReferenceString.subList(i, -1).lastIndexOf(pageReferenceString.get(i));
+            
+        // }
+        int frameToReplace;
+        List<Integer> indicesOfLastOccurrence = new ArrayList<>(frames.size());
+        for (int i = 0; i < frames.size(); i++) 
+        {
+            indicesOfLastOccurrence.add(pageReferenceString.lastIndexOf(frames.get(i)));
+        }
+        if (indicesOfLastOccurrence.contains(-1)) 
+        {
+            frameToReplace = indicesOfLastOccurrence.indexOf(-1);
+        } 
+        else 
+        {
+            frameToReplace = indicesOfLastOccurrence.indexOf(Collections.max(indicesOfLastOccurrence));
+        }
+        return frames.get(frameToReplace);
+    }
+
     @Override
-    public int getPageFaults() {
+    public int getPageFaults() 
+    {
         return pageFaults;
     }
 }
